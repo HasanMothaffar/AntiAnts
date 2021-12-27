b4fcc53db388e4802005907303a4c61d1d2e1eff
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-12-27 16:12:33 +0300

    Separate declaration from implementation of bullet files

--- bullet.h
+++ bullet.h
@@ -6,6 +6,9 @@
 #include "skybox.h"
 
 class Bullet {
+private:
+	void shift();
+
 public:
 	Vector3dStruct position;
 	Vector3dStruct view;
@@ -13,50 +16,15 @@ public:
 
 	float speed;
 
-	static Bullet* createBullet(const Camera *camera, float speed = 6.0f) {
-		Bullet * bullet = new Bullet;
-		bullet->position = camera->Position;
-		bullet->view = camera->View;
-		bullet->real = Vector3dCreate(
-			camera->Position.x + camera->View.x,
-			camera->Position.y + camera->View.y,
-			camera->Position.z + camera->View.z
-		);
-		bullet->speed = speed;
+	static Bullet* createBullet(const Camera *camera, float speed = 6.0f);
 
-		return bullet;
-	}
-
-	void draw() {
-		glPushMatrix();
-		glColor3f(0, 0, 1);
-		glPointSize(20);
-		glBegin(GL_POINTS);
-		glVertex3f(real.x, real.y, real.z);
-		glEnd();
-		shift();
-		glPopMatrix();
-	}
+	void draw();
 	
 	/*
 		Returns whether the bullet has exceeded
 		the limits (faces) of the skybox. 
 	*/
-	bool isOutOfBoundaries(const Skybox &skybox) {
-		return (
-			real.z < -skybox.length || // Back face
-			real.z > 0 || // Front face
-			real.x > skybox.width || // Right face
-			real.x < -skybox.width // Left face
-		);
-	}
-
-private:
-	void shift() {
-		real.x += view.x * speed;
-		real.y += view.y * speed;
-		real.z += view.z * speed;
-	}
+	bool isOutOfBoundaries(const Skybox &skybox);
 };
 
 #endif
